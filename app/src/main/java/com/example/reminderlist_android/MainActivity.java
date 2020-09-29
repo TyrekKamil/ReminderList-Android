77bd38397fb5bdcd8f56f3bf46d7489afcb619e5
Kamil Tyrek <tyrekkamil@gmail.com>
2020-09-29 21:59:13 +0200

    Saving checkbox values

--- app/src/main/java/com/example/reminderlist_android/MainActivity.java
+++ app/src/main/java/com/example/reminderlist_android/MainActivity.java
@@ -1,33 +1,42 @@
 package com.example.reminderlist_android;
 
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Layout;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Checkable;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Vector;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
 
     private LinearLayout linearLayout;
     public static final String SHARED_PREFS = "sharedPrefs";
     private List<String> taskForPrefs;
+    private HashMap<String, Boolean> activeTasks;
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,33 +52,42 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 EditText text = findViewById(R.id.elementName);
-                addToList(text.getText().toString(), false);
+                addToList(text.getText().toString(), false, false);
             }
         });
     }
 
-    private void addToList(final String text, Boolean initial) {
+    private void addToList(final String text, Boolean initial, Boolean isChecked) {
 
         final TextView textView = new TextView(MainActivity.this.getApplicationContext());
         final Button deleteBtn = new Button(MainActivity.this.getApplicationContext());
+        final CheckBox checkBox = new CheckBox(MainActivity.this.getApplicationContext());
         final LinearLayout row = new LinearLayout(MainActivity.this.getApplicationContext());
 
-        if(!taskForPrefs.contains(text) || initial) {
+        if(isChecked) {
+            checkBox.setChecked(true);
+        } else {
+            checkBox.setChecked(false);
+        }
+
+        if ((!taskForPrefs.contains(text) || initial) && text != "") {
             Log.i("ReminderList-App", "Add element" + text);
             row.setLayoutParams(new LinearLayout.LayoutParams
                     (LinearLayout.LayoutParams.WRAP_CONTENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT,
                             LinearLayout.LayoutParams.WRAP_CONTENT));
 
             deleteBtn.setText("X");
             textView.setText(text);
             row.addView(textView);
             row.addView(deleteBtn);
+            row.addView(checkBox);
             row.setId(taskForPrefs.size());
 
             linearLayout.addView(row);
 
             if(!initial) {
-                saveData(text.trim());
+                saveName(text.trim());
             }
 
             deleteBtn.setOnClickListener(new View.OnClickListener() {
@@ -78,8 +96,15 @@ public class MainActivity extends AppCompatActivity {
                     removeFromList(row, text);
                 }
             });
+
+            checkBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    saveChecked(checkBox.isChecked(), text.trim());
+                }
+            });
         } else {
-            Toast.makeText(getApplicationContext(), "Element exists: " + text, Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Element exists or empty", Toast.LENGTH_SHORT).show();
         }
 
 
@@ -89,10 +114,10 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), "Element Removed: " + text, Toast.LENGTH_SHORT).show();
         linearLayout.removeView(row);
         taskForPrefs.remove(text);
-        saveData("");
+        saveName("");
     }
 
-    private void saveData(String taskName) {
+    private void saveName(String taskName) {
         if(taskName != "") {
             taskForPrefs.add(taskName);
         }
@@ -103,18 +128,29 @@ public class MainActivity extends AppCompatActivity {
         Log.i("SharedPrefs: " , taskForPrefs.toString());
     }
 
+    private void saveChecked(Boolean isChecked, String taskName) {
+        editor.putBoolean(taskName, isChecked);
+        editor.apply();
+        Log.i("ShaderPrefs: ", taskName + ": " + isChecked);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     private void loadData() {
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         taskForPrefs = new ArrayList<String>(Arrays.asList(sharedPreferences.getString("reminderList", "").split(",")));
+        activeTasks = new HashMap<String, Boolean>();
         Log.i("ReminderList-App", "HERE WE GO AGAIN " + taskForPrefs);
-
+        Boolean isChecked = false;
         for (int i = 0; i < taskForPrefs.size(); i++) {
-            addToList(taskForPrefs.get(i), true);
+            isChecked = sharedPreferences.getBoolean(taskForPrefs.get(i).trim(), false);
+            activeTasks.put(taskForPrefs.get(i), isChecked);
+            addToList(taskForPrefs.get(i), true, isChecked);
         }
     }
 
     private void clearPrefs() {
         editor.putString("reminderList", "");
+        // TODO clear shaderPrefs booleans, all arrays etc
         editor.apply();
     }
 
